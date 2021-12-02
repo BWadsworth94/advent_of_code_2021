3d68a849f157a8d48a2259ea5272cf32ae071fc4
Ben Wadsworth <bwadsworth@splashfinancial.com>
2021-12-02 09:05:23 -0500

    part 2

--- src/day1/core.clj
+++ src/day1/core.clj
@@ -6,7 +6,7 @@
        (clojure.string/split-lines)
        (map #(Integer/parseInt %))))
 
-(defn count-increases
+(defn part-1-count-increases
   [input]
   (loop [input input
          acc 0]
@@ -14,3 +14,7 @@
       (if (nil? s)
         acc
         (recur (rest input) (if (> s f) (+ 1 acc) acc))))))
+
+(defn part-2-count-increases
+  [input]
+  )
