6dcc4bd53a8c6f38a940e9274f033519d68b92f0
Ben Wadsworth <wadsworth.benjamin.m@gmail.com>
2021-12-03 21:31:42 -0500

    day 3

--- src/day3/core.clj
+++ src/day3/core.clj
@@ -1,50 +1,31 @@
-(ns day3.core)
+(ns day3.core
+  (:require [clojure.string :as s]))
 
 (def input
   (-> "resources/day3/input.txt"
       slurp
-      clojure.string/split-lines))
+      s/split-lines))
 
-(defn ->index-mapping
-  [instruction]
-  (->> instruction
-       (map-indexed (fn [idx item]
-                      {idx [item]}))
-       (apply merge)))
+(defn transpose
+  [vectors]
+  (apply mapv vector vectors))
 
-(defn ->indexed-binary-values
-  [binary-instructions]
-  (reduce (fn [acc i]
-            (merge-with into acc (->index-mapping i))) 
-          {} 
-          binary-instructions))
-
-(defn ->indexed-binary-frequencies
-  [m]
-  (mapv (fn [[idx binary-values]]
-         {idx (frequencies binary-values)}) m))
-
-(defn most-common-binary
-  [m]
-  (let [freq-zeros (get m \0 0)
-        freq-ones  (get m \1 0)]
-    (if (> freq-zeros freq-ones) 0 1)))
+(defn get-num
+  [input most-common-fn]
+  (->> input
+       (map #(s/split % #"")) ;; ["1001"] -> ["1" "0" "0" "1"]
+       (map (partial map #(s/replace % #"0" "-1"))) ;; ["1" "0" "0" "1"] -> ["1" "-1" "-1" "1"]
+       (map (partial map #(Integer/parseInt %))) ;; str to int
+       transpose ;; [[1 0 1] [1 1 0]] -> [[1 1] [0 1] [1 0]]
+       (map #(apply + %)) ;; add the vectors
+       (map (fn [b] (if (most-common-fn b) 1 0))) ;; if adds to pos or negative give 1 or 0
+       s/join ;; smash the vector into a string
+       (#(Integer/parseInt % 2)))) ;; convert it to dec
 
 (defn part-1
-  [input use-least-common?]
-  (->> input
-       ->indexed-binary-values
-       ->indexed-binary-frequencies
-       (map (fn [freq-mappings]
-              (let [binary-counts-map (first (vals freq-mappings))
-                    most-common (most-common-binary binary-counts-map)]
-                (if use-least-common?
-                  (- 1 most-common)
-                  most-common)
-               )))
-       clojure.string/join
-       (#(Integer/parseInt % 2))))
+  [input]
+  {:gamma (get-num input pos-int?)
+   :epsilon (get-num input (complement pos-int?))})
 
 (comment
-  (part-1 input true)
-  (part-1 input false)
+  (part-1 input))
